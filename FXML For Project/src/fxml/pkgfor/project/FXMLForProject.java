a861b1209341747101fa847f4dc9716bc8d46485
Sadman Sakib <2221929@iub.edu.bd>
2023-12-01 14:10:28 +0600

    minor updates

--- FXML For Project/src/fxml/pkgfor/project/FXMLForProject.java
+++ FXML For Project/src/fxml/pkgfor/project/FXMLForProject.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -27,6 +28,8 @@ public class FXMLForProject extends Application {
         
         stage.setScene(scene);
         stage.show();
+        stage.setTitle("BGMEA");
+        stage.getIcons().add(new Image("file:bgmea_logo.png"));
     }
 
     /**
--- FXML For Project/src/fxml/pkgfor/project/SampleOrderbinaryfileController.java
+++ FXML For Project/src/fxml/pkgfor/project/SampleOrderbinaryfileController.java
@@ -26,12 +26,12 @@ public class SampleOrderbinaryfileController implements Initializable {
     @FXML
     private void createBinFile(ActionEvent event) {
         List<Login> dataList = new ArrayList<>();
-        dataList.add(new Login(5198, "1234abcd"));
-        dataList.add(new Login(4971, "dcba4321"));
-        dataList.add(new Login(7954, "abcd1234"));
-        dataList.add(new Login(4444, "password"));
+        dataList.add(new Login(4718, "1234abcd"));
+        dataList.add(new Login(7653, "dcba4321"));
+        dataList.add(new Login(6738, "abcd1234"));
+        dataList.add(new Login(8888, "password"));
 
-        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("QualityControlInspectorLoginInfo.bin"))) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("HumanResourcesManagerLoginInfo.bin"))) {
             oos.writeObject(dataList);
             System.out.println("Cost Report.bin file created successfully.");
         } catch (Exception e) {
